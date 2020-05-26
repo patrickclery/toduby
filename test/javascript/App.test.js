0337f199e49227b3fea932c10e94431fd1c6186f
patrickclery <patrick.clery@gmail.com>
2020-05-26 08:50:58 -0700

    :construction: Work in progress.

--- test/javascript/App.test.js
+++ test/javascript/App.test.js
@@ -17,11 +17,46 @@ import App from '../../app/javascript/components/App.jsx'
 describe('<App />', () => {
   beforeEach(() => {
     // if you have an existing `beforeEach` just add the following line to it
-    fetch.mockResponse(JSON.stringify({access_token: '12345'}))
+    const stubData = [
+      {
+        "id":         "1",
+        "type":       "todo",
+        "attributes": {
+          "description": "Brush teeth",
+          "priority":    "0",
+          "completedAt": nil,
+          "createdAt":   "2020-03-03",
+          "updatedAt":   "2020-03-03"
+        }
+      },
+      {
+        "id":         "2",
+        "type":       "todo",
+        "attributes": {
+          "description": "Feed cat",
+          "priority":    "1",
+          "completedAt": nil,
+          "createdAt":   "2020-03-03",
+          "updatedAt":   "2020-03-03"
+        }
+      },
+      {
+        "id":         "3",
+        "type":       "todo",
+        "attributes": {
+          "description": "Pickup laundry",
+          "priority":    "0",
+          "completedAt": "2020-05-09",
+          "createdAt":   "2020-03-03",
+          "updatedAt":   "2020-03-03"
+        }
+      }
+    ]
+    fetch.mockResponse(JSON.stringify({data: stubData}))
   })
 
   it('fetches the list of todos', () => {
     const wrapper = shallow(<App/>); // 5
-    expect(wrapper.text()).to.contain('todo')
+    expect(wrapper.find('tbody>tr>td')).toHaveLength(3)
   })
 })
