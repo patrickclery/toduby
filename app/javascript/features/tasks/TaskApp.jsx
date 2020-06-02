4abded132a4f683c3089e8046318560677512203
patrickclery <patrick.clery@gmail.com>
2020-06-01 19:07:35 -0700

    works

--- app/javascript/features/tasks/TaskApp.jsx
+++ app/javascript/features/tasks/TaskApp.jsx
@@ -1,12 +1,16 @@
 import React, {useEffect} from "react"
 import TaskTable from "./TaskTable"
-import {fetchTasks as fetchTasksReducer} from "./tasksSlice"
+import {fetchTasks} from "./tasksSlice"
 import {useDispatch, useSelector} from "react-redux"
 
 const TaskApp = props => {
   const baseUrl = props.baseUrl
   const tasks = useSelector(state => state.tasks.entities)
+  const dispatch = useDispatch()
 
+  useEffect(() => {
+    dispatch(fetchTasks(baseUrl))
+  }, [])
   return (
     <TaskTable tasks={tasks}/>
   )
