bf30c5d781bc7e72c711f5086193fe4b0b3043e6
patrickclery <patrick.clery@gmail.com>
2020-06-05 19:59:24 -0700

    :construction: Work in progress.

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -3,22 +3,6 @@ const path = require("path")
 
 module.exports = {
   entry:     "./src/index.js",
-  resolve:   {
-    extensions: ["*", ".js", ".jsx"]
-  },
-  output:    {
-    path:       path.resolve(__dirname, "dist"),
-    publicPath: "/",
-    filename:   "bundle.js"
-  },
-  plugins:   [
-    new webpack.HotModuleReplacementPlugin()
-  ],
-  devServer: {
-    contentBase: "./dist",
-    hot:         true,
-    index:       "index.html"
-  },
   module:    {
     rules: [
       {
