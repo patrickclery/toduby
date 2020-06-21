9ea153003b55c23c3e05c96b25563c9d902e64c1
patrickclery <patrick.clery@gmail.com>
2020-06-21 14:21:18 -0700

    ˆ :lipstick: Updating the UI and style files.

--- client/src/components/TaskForm.js
+++ client/src/components/TaskForm.js
@@ -2,19 +2,14 @@ import React from "react"
 import {useDispatch, useSelector} from "react-redux"
 import {changeTaskInput, createTask} from "../slices/tasksSlice"
 import * as PropTypes from "prop-types"
-import {styled} from "twin.macro"
-
-const Container = styled.div`
-  width: auto;
-  grid-template-columns: auto minmax(auto, 100px) minmax(auto, 100px);
-  display: grid;
-  grid-column-gap: 10px;
-`
+import tw, {styled} from "twin.macro"
 
 function TaskForm() {
   const description = useSelector(state => state.tasks.task.description)
   const priority = useSelector(state => state.tasks.task.priority)
   const dispatch = useDispatch()
+
+  const Container = tw.div`max-w-screen-sm grid grid-cols-5 gap-2`
 
   const handleSubmit = async (event) => {
     event.preventDefault()
@@ -37,25 +32,25 @@ function TaskForm() {
   const DescriptionInput = props => <input
     defaultValue={props.defaultValue}
     name="todo[description]"
-    placeholder="Type the task you'd like to do next here"
-    tw="grid text-lg w-auto"
+    placeholder="Type something (example: 'Do fifty pushups')"
+    tw="grid col-span-3 text-sm w-auto"
     type="text"
   />
   DescriptionInput.propTypes = {defaultValue: PropTypes.string}
 
   const PrioritySelect = props => <select
     name="todo[priority]"
+    tw="text-lg grid col-span-1"
     defaultValue={props.defaultValue}
   >
     <option value="0">Low</option>
     <option value="1">Medium</option>
     <option value="2">High</option>
   </select>
-
-  PrioritySelect.propTypes = {defaultValue: PropTypes.any}
+  PrioritySelect.propTypes = {defaultValue: PropTypes.number}
 
   const SubmitButton = () => <button
-    tw="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
+    tw="col-span-1 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
     type="submit"
   >Create</button>
 
